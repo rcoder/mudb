37f6209f44953ac5f737600397439bc5420de551
Lennon Day-Reynolds <lennon@rcoder.net>
2022-07-27 12:25:19 -0700

    chore: fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,11 @@
 use anyhow::Result;
 use cap_std::fs::{Dir, File, OpenOptions};
+use cap_tempfile::TempFile;
 use rusty_ulid::generate_ulid_string;
-use serde::{Serialize, Deserialize};
 use serde::de::DeserializeOwned;
-use serde_json::{json,Value};
+use serde::{Deserialize, Serialize};
 use std::collections::{BTreeMap, HashSet};
-use std::fmt::Debug;
-use std::io::{BufRead, BufReader, BufWriter, Seek, SeekFrom, Write};
-use std::ops::Deref;
+use std::io::{BufReader, BufWriter, Seek, SeekFrom, Write};
 use std::rc::Rc;
 
 fn default_open_options() -> OpenOptions {
@@ -33,7 +31,7 @@ pub struct Doc<T: Clone> {
     flags: HashSet<Flag>,
 }
 
-impl <T: Serialize + DeserializeOwned + Clone> Doc<T> {
+impl<T: Serialize + DeserializeOwned + Clone> Doc<T> {
     pub fn new(id: String, obj: Option<T>) -> Self {
         Self {
             id,
@@ -53,9 +51,10 @@ pub struct Mudb<T: Serialize + DeserializeOwned + Clone> {
     filename: String,
     write_fh: BufWriter<File>,
     data: BTreeMap<String, Doc<T>>,
+    modified: bool,
 }
 
-impl <T: Serialize + DeserializeOwned + Clone> Mudb<T> {
+impl<T: Serialize + DeserializeOwned + Clone> Mudb<T> {
     pub fn open(data_dir: Rc<Dir>, filename: &str) -> Result<Self> {
         let mut file = data_dir.open_with(filename, &default_open_options())?;
         let mut data = BTreeMap::new();
@@ -78,35 +77,77 @@ impl <T: Serialize + DeserializeOwned + Clone> Mudb<T> {
             filename: filename.to_string(),
             write_fh: BufWriter::new(file),
             data,
+            modified: false,
         })
     }
 
     pub fn insert(&mut self, id: Option<String>, obj: T) -> Result<String> {
         let id = id.unwrap_or_else(|| generate_ulid_string());
 
-        let mut doc = self.data.entry(id.clone()).or_insert(Doc::new(id.clone(), None));
+        let mut doc = self
+            .data
+            .entry(id.clone())
+            .or_insert(Doc::new(id.clone(), None));
         doc.obj = Some(obj);
+        self.modified = true;
 
-        write!(self.write_fh, "{}\n", serde_json::to_string(&doc)?)?;
+        write!(&mut self.write_fh, "{}\n", serde_json::to_string(doc)?)?;
         self.write_fh.flush()?;
 
         Ok(id)
     }
 
     pub fn get(&self, id: String) -> Option<T> {
-        self.data.get(&id).iter().flat_map(|doc| doc.obj.clone()).next()
+        self.data
+            .get(&id)
+            .iter()
+            .flat_map(|doc| doc.obj.clone())
+            .next()
+    }
+
+    pub fn delete(&mut self, id: String) -> Result<Option<T>> {
+        let found = self.data.remove(&id);
+
+        if let Some(mut doc) = found {
+            let obj = doc.obj;
+            doc.obj = None;
+            doc.flags.insert(Flag::Deleted);
+            write!(&mut self.write_fh, "{}\n", serde_json::to_string(&doc)?)?;
+            self.write_fh.flush()?;
+            self.data.insert(id.clone(), doc);
+            self.modified = true;
+            Ok(obj)
+        } else {
+            Ok(None)
+        }
+    }
+
+    pub fn compact(&mut self) -> Result<()> {
+        if self.modified {
+            let mut tmpf = TempFile::new(&mut self.data_dir)?;
+            for (_key, val) in self.data.iter() {
+                write!(tmpf, "{}\n", serde_json::to_string(val)?)?;
+            }
+
+            tmpf.replace(&self.filename)?;
+            let write_fh = self.data_dir.open(&self.filename)?;
+
+            self.write_fh = BufWriter::new(write_fh);
+            self.modified = false;
+        }
+
+        Ok(())
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::Mudb;
+    use anyhow::Result;
     use cap_std::ambient_authority;
     use cap_std::fs::Dir;
     use cap_tempfile::TempDir;
-    use anyhow::Result;
-    use serde::{Serialize, Deserialize};
-    use std::ops::Deref;
+    use serde::{Deserialize, Serialize};
     use std::rc::Rc;
 
     const DATA_DIR: &str = ".data";
@@ -121,10 +162,7 @@ mod test {
     #[derive(Serialize, Deserialize, Clone, PartialEq, Eq, Debug)]
     enum TestMessage {
         Empty,
-        Of {
-            kind: u16,
-            val: String,
-        },
+        Of { kind: u16, val: String },
     }
 
     #[test]
@@ -135,7 +173,10 @@ mod test {
         let key1 = "key1".to_string();
         let key2 = "key2".to_string();
         let msg1 = TestMessage::Empty;
-        let msg2 = TestMessage::Of { kind: 1, val: "just passing through".to_string() };
+        let msg2 = TestMessage::Of {
+            kind: 1,
+            val: "just passing through".to_string(),
+        };
 
         {
             let mut db: Mudb<TestMessage> = Mudb::<TestMessage>::open(dd_rc.clone(), "_test")?;
@@ -152,6 +193,32 @@ mod test {
             assert_eq!(db.get(key1.clone()), Some(msg1));
             assert_eq!(db.get(key2.clone()), Some(msg2));
         }
+
+        Ok(())
+    }
+
+    #[test]
+    fn compact() -> Result<()> {
+        let (_tmp, data_dir) = data_dir()?;
+        let dd_rc = Rc::new(data_dir);
+
+        let key = "key1".to_string();
+        let msg = TestMessage::Of {
+            kind: 0,
+            val: "so meta".to_string(),
+        };
+        let msg2 = TestMessage::Empty;
+
+        let mut db = Mudb::<TestMessage>::open(dd_rc.clone(), "_test")?;
+        let _ = db.insert(Some(key.clone()), msg.clone());
+        assert_eq!(db.get(key.clone()), Some(msg.clone()));
+
+        let _ = db.insert(Some(key.clone()), msg2.clone())?;
+        assert_eq!(db.get(key.clone()), Some(msg2.clone()));
+
+        let _ = db.compact()?;
+
+        assert_eq!(db.get(key), Some(msg2));
 
         Ok(())
     }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use cap_tempfile::TempDir;
+use clap::{Parser, Subcommand};
+use mudb::Mudb;
+use std::path::PathBuf;
+use std::rc::Rc;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short, long, value_parser, value_name = "FILE")]
+    data_dir: Option<PathBuf>,
+
+    #[clap(subcommand)]
+    command: Option<Op>,
+}
+
+#[derive(Subcommand)]
+enum Op {
+    Check,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match &args.command {
+        Some(Op::Check) => {
+            let tmpd = TempDir::new(cap_std::ambient_authority()).unwrap();
+            let _ = tmpd.create_dir("tmp").unwrap();
+            let data = tmpd.open_dir("tmp").unwrap();
+            let db = Mudb::<String>::open(Rc::new(data), "check");
+        }
+        None => {}
+    }
 }
